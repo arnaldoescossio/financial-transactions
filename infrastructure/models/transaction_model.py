7cecf7dc2bbc64a269bfbad5714526ef53582815
Arnaldo Escossio <arnaldoescossio@gmail.com>
2026-03-07 00:02:14 -0500

    Configure migration by alembic

--- infrastructure/models/transaction_model.py
+++ infrastructure/models/transaction_model.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, Float, String
+from sqlalchemy import Integer, Float, String, DateTime, func
 
 from infrastructure.database import Base
 
@@ -10,3 +11,6 @@ class TransactionModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     amount: Mapped[float] = mapped_column(Float, nullable=False)
     status: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    
